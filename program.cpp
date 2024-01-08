f1c4d79d475ca02bb46155c6bdfc8e510130c047
Mohammad Nickmagham <you@example.com>
2024-01-08 12:53:32 +0330

    add recognizing user choice algorithm

--- program.cpp
+++ program.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 int main()
 {
+    srand(time(NULL));
+    int random = (rand() % 3) + 1;
+    string choosed;
+    string rps[4] = {" ", "rock", "paper", "scissors"};
+    int choosedNum;
+
+    cout << "Choose by number :" << endl
+         << "1.Rock   2.Paper   3.Scissors" << endl;
+    cin >> choosedNum;
+    for (int i = 1; i < 4; i++)
+    {
+        if (choosedNum != i)
+        {
+            continue;
+        }
+        else
+        {
+            choosed = rps[i];
+        }
+    }
+    cout << choosed;
 }
